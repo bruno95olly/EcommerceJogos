d11f5803bd11d1f9fe7710ceff042dd9cfb30f5e
bruno95olly <89094799+bruno95olly@users.noreply.github.com>
2021-10-06 18:55:40 -0300

    Delete placeholderLoading.js

--- js/placeholderLoading.js
+++
@@ -1,8 +0,0 @@
-const imgContainer = document.querySelectorAll('.img-produto');
-const heading = document.querySelectorAll('.heading');
-const para = document.querySelectorAll('.para')
-
-setTimeout(()=>{
-  imgContainer.forEach((ic)=> ic.classList.remove("loading") )
-}, 2000);
-
